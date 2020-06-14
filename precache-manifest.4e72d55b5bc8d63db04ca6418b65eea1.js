efb0e419c4920239c15adc0de77e326e02be13c8
naengsoodosa <naengsoo.dosa@gmail.com>
2020-06-14 17:15:40 +0900

    Updates

---
+++ precache-manifest.4e72d55b5bc8d63db04ca6418b65eea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6496b018666f233c33101ce2aee0929",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c626825b9e070b8d5fb8",
+    "url": "/movie_app_2020/static/css/main.96625c1b.chunk.css"
+  },
+  {
+    "revision": "b17fb38856ffb36d44ef",
+    "url": "/movie_app_2020/static/js/2.14c54020.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.14c54020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c626825b9e070b8d5fb8",
+    "url": "/movie_app_2020/static/js/main.20e11be6.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
